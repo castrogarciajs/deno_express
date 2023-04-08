38f601d399dd1edbaa95711e7eb8006fde2aa966
Sebastian009w <ia3266595@gmail.com>
2023-04-07 21:57:30 -0500

    se definen los controladores del proyecto

--- src/controllers/controllers.ts
+++ src/controllers/controllers.ts
@@ -1,3 +1,17 @@
-export const __init__ = (req, res) => {
+import { Request, Response } from "npm:express";
+
+export const __init__ = (_req: Request, res: Response) => {
   res.send("hello");
+};
+
+export const create_task = (_req: Request, res: Response) => {
+  res.send("creando tarea");
+};
+
+export const update_task = (_req: Request, res: Response) => {
+  res.send("update tarea");
+};
+
+export const delete_task = (_req: Response, res: Response) => {
+  res.send("eliminando tarea");
 };
--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "npm:express";
-import { __init__ } from "../controllers/controllers.ts";
+import {
+  __init__,
+  create_task,
+  update_task,
+  delete_task,
+} from "../controllers/controllers.ts";
 
 const router = Router();
 
 router.get("/", __init__);
+router.post("/", create_task);
+router.put("/", update_task);
+router.delete("/", delete_task);
 
 export default router;
